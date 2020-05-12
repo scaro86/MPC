7837b8eed42ca1932d6d06a57fe5efd236f99540
scaro <csauget34@gmail.com>
2020-05-12 15:07:17 +0200

    details, still not correct

--- controller_mpc_5.m
+++ controller_mpc_5.m
@@ -12,10 +12,8 @@ persistent param yalmip_optimizer T_hat d_hat u
 % initialize controller, if not done already
 if isempty(param)
     [param, yalmip_optimizer] = mpc_5_optimizer();
-    x = T;
-    T_hat = T;
     d_hat = param.d;
-    u = zeros(2,1);
+    u = ones(2,1);
 end
 %get the estimation 
 Nsim = 10;
@@ -29,31 +27,20 @@ C_aug = param.C_aug;
 d = param.d;
 L = param.L;
 %Simulate autonomous system
+T_hat = T;
 aux = [T_hat; d_hat];
 x = T;
 for i = 1:Nsim-1
     x(:,i+1) = A*x(:,i) + B*u+ Bd*d;
     aux = A_aug*aux + B_aug*u+ L*C_aug*(aux - [x(:,i);d]);
-    T_hat(:,i+1) = aux(1:3); 
-    d_hat(:,i+1) = aux(4:6);
 end
-T_hat = T_hat(:,end);
-d_hat = d_hat(:,end);
+T_hat = aux(1:3);
+d_hat = aux(4:6);
 % calculate steady state
 
 T_sp = param.T_sp; % for T1 and T2 we track the same steady state as before
 [T_sp, p_sp] = steady(A, B, Bd, T_sp, d_hat); 
 
-% H = eye(3);
-% r = [0,0,0]';
-% left = [A-eye(3) B;...
-%     H*C zeros(3,2)];
-% right = [-Bd*d_hat;...
-%         r];
-% estim = left\right;
-% T_sp = param.T_sp;
-% T_sp(3) = estim(3);
-% p_sp = estim(4:end);
 % get x0
 x0 = T - T_sp;
 % get optimal u
