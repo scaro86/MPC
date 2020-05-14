16dc70108435b18b558367c4b6d48389109e6b71
scaro <csauget34@gmail.com>
2020-05-14 19:44:05 +0200

    final

--- compute_controller_base_parameters.m
+++ compute_controller_base_parameters.m
@@ -46,7 +46,7 @@ function param = compute_controller_base_parameters
     %(21)
 %     L = -(place(A_aug', C_aug', [0,0.35,0.1,0.2,0.4,0.5]))';
 %     L = -(place(A_aug', C_aug', [0,0.3,0.1,0,0.4,0.5]))';
-    L = -(place(A_aug', C_aug', [0.98,0.98,0.9,0.8,0.9,0.95]))';
+    L = -(place(A_aug', C_aug', [0.3,0.2,0.2,0.2,0.3,0.3]))';
     eig(A_aug + L*C_aug)
     
     % put everything together
