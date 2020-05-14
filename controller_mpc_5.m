8a81082011f6ce1c90e4d4a1d9ffaa64fa113c93
scaro <csauget34@gmail.com>
2020-05-14 16:07:48 +0200

    exo22 final

--- controller_mpc_5.m
+++ controller_mpc_5.m
@@ -35,23 +35,22 @@ est = A_aug*est+B_aug*p_est+L*C_aug*(est-[T; param.d]);
 T_hat = est(1:3);
 d_hat = [d_hat, est(4:6)];
 % plot disturbance estimation
-subplot(3,1,1)
-plot(d_hat(1,:));
-subplot(3,1,2)
-plot(d_hat(2,:));
-subplot(3,1,3)
-plot(d_hat(3,:));
-err = abs(d_hat(:,end)-d_hat(:,end-1));
-if (err <= 1)
-    err = abs(d_hat-d_hat(:,end));
-    err1 = max(err(1,:));
-    err2 = max(err(2,:));
-    err3 = max(err(3,:));
-end
+% subplot(3,1,1)
+% plot(d_hat(1,:));
+% subplot(3,1,2)
+% plot(d_hat(2,:));
+% subplot(3,1,3)
+% plot(d_hat(3,:));
+% err = abs(d_hat(:,end)-d_hat(:,end-1));
+% if (err <= 1)
+%     err = abs(d_hat-d_hat(:,end));
+%     err1 = max(err(1,:));
+%     err2 = max(err(2,:));
+%     err3 = max(err(3,:));
+% end
 % calculate steady state
 T_sp = param.T_sp; % for T1 and T2 we track the same steady state as before
-% [T_sp, p_sp] = steady(param.A, param.B, param.Bd, T_sp, d_hat(:,end)); %calculate T_sp(3) and p_sp
-[T_sp, p_sp] = steady_hat(param.A, param.B, param.Bd,param.C, T_sp, d_hat(:,end))
+[T_sp, p_sp] = steady(param.A, param.B, param.Bd, T_sp, d_hat(:,end)); %calculate T_sp(3) and p_sp
 %get constraints
 % get x0
 x0 = T - T_sp;
